284a159d0be74b0bb2c2619cde9a7c9f56563874
a_happin <10003601+a@users.noreply.github.com>
2021-09-28 10:42:11 +0900

    docs: Add comment

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -12,6 +12,7 @@ if status is-login
     set -gx VISUAL nvim
   end
 
+  # XDG Base Directory
   set -gx XDG_CONFIG_HOME "$HOME/.config"
   set -gx XDG_CACHE_HOME "$HOME/.cache"
   set -gx XDG_DATA_HOME "$HOME/.local/share"
