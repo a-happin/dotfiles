8dd63d0a50cc41d62bcda0c183a57bff137c706a
a_happin <10003601+a@users.noreply.github.com>
2022-08-22 23:53:23 +0900

    Fix template file of c++

--- home/.config/nvim/template/.cpp
+++ home/.config/nvim/template/.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-#define _for(i,b,e) for (common_type_t<decltype(b),decltype(e)> i = (b), i ## __end = (e); i < i ## __end; ++ i)
-#define FOR(...)   _for (__VA_ARGS__)
-#define rep(i,n)   FOR (i, size_t{}, n)
+#define FOR_IMPL(i,b,e) for (auto [i, i ## __end] = tuple {(b), (e)}; i < i ## __end; ++ i)
+#define FOR(...)   FOR_IMPL (__VA_ARGS__)
+#define rep(i,n)   FOR (i, size_t{}, static_cast <size_t> (n))
 #define ALL(x)     begin (x), end (x)
 using namespace std;
 using ll = int64_t;
