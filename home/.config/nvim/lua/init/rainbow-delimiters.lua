1f4c0f8ede57bf54edc166ede0b274b1aff3b4d5
a_happin <10003601+a@users.noreply.github.com>
2024-09-02 18:29:58 +0900

    Fix nvim config

--- home/.config/nvim/lua/init/rainbow-delimiters.lua
+++ home/.config/nvim/lua/init/rainbow-delimiters.lua
@@ -33,6 +33,13 @@ local define_highlight = function ()
   vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
   vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
   vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+
+  vim.api.nvim_set_hl (0, "RainbowRed2", { fg = "#bf616a" })
+  vim.api.nvim_set_hl (0, "RainbowOrange2", { fg = "#d08770" })
+  vim.api.nvim_set_hl (0, "RainbowYellow2", { fg = "#ebcb8b" })
+  vim.api.nvim_set_hl (0, "RainbowGreen2", { fg = "#a3be8c" })
+  vim.api.nvim_set_hl (0, "RainbowBlue2", { fg = "#5e81ac" })
+  vim.api.nvim_set_hl (0, "RainbowViolet2", { fg = "#b48ead" })
 end
 
 --- @type fun(group: string, f: fun(autocmd: fun(event: string | string[], opts: {group: string | integer, pattern: string | string[], callback: (fun(_: any): any), buffer: integer, desc: string, command: string, once: boolean, nested: boolean }): number): nil): nil
@@ -48,14 +55,25 @@ augroup ('fix-rainbow-delimiters', function (autocmd)
   autocmd ({"ColorScheme"}, {callback = define_highlight})
 end)
 
+-- vim.g.rainbow_delimiters = {
+--     highlight = {
+--         'RainbowRed',
+--         'RainbowYellow',
+--         'RainbowBlue',
+--         'RainbowOrange',
+--         'RainbowGreen',
+--         'RainbowViolet',
+--         'RainbowCyan',
+--     },
+-- }
+
 vim.g.rainbow_delimiters = {
-    highlight = {
-        'RainbowRed',
-        'RainbowYellow',
-        'RainbowBlue',
-        'RainbowOrange',
-        'RainbowGreen',
-        'RainbowViolet',
-        'RainbowCyan',
-    },
+  highlight = {
+    'RainbowRed2',
+    'RainbowOrange2',
+    'RainbowYellow2',
+    'RainbowGreen2',
+    'RainbowBlue2',
+    'RainbowViolet2',
+  }
 }
